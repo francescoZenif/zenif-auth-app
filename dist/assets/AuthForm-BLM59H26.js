685e7f77114f2ba4d34e08152b66ec9d1502ecf0
francesco <138888743+francescoZenif@users.noreply.github.com>
2026-04-27 18:03:46 +0100

    wip

---
+++ dist/assets/AuthForm-BLM59H26.js
@@ -0,0 +1,2 @@
+import{e as m,o as r,A as n,B as a}from"./index-D_ycU2nR.js";const u={__name:"AuthForm",emits:["submit"],setup(i,{emit:e}){const t=e;function o(){t("submit")}return(s,c)=>(r(),m("form",{class:"p-0 flex flex-col gap-5 m-5",onSubmit:a(o,["prevent"])},[n(s.$slots,"default")],32))}};export{u as _};
+//# sourceMappingURL=AuthForm-BLM59H26.js.map
