93d2f7833b8f5562a3a57542bf43e7035bcf69a8
francesco <138888743+francescoZenif@users.noreply.github.com>
2026-05-12 10:44:08 +0100

    wip

--- dist/assets/Logout-BLEdO1ob.js
+++
@@ -1,2 +0,0 @@
-import{C as e}from"./index-buERM-Or.js";const n={__name:"Logout",setup(t){return e(()=>{localStorage.getItem("zenif_auth_redirect")&&localStorage.removeItem("zenif_auth_redirect"),window.location.href="/"}),(o,r)=>null}};export{n as default};
-//# sourceMappingURL=Logout-BLEdO1ob.js.map
---
+++ dist/assets/Logout-BWhjqVd_.js
@@ -0,0 +1,2 @@
+import{C as e}from"./index-CkijMoK7.js";const n={__name:"Logout",setup(t){return e(()=>{localStorage.getItem("zenif_auth_redirect")&&localStorage.removeItem("zenif_auth_redirect"),window.location.href="/"}),(o,r)=>null}};export{n as default};
+//# sourceMappingURL=Logout-BWhjqVd_.js.map
